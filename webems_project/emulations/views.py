527df0933a258238ff24c38eb7b7f65f08195d65
Kevin Doran <kev.doran@gmail.com>
2012-10-27 16:36:12 -0500

    basic view emulations

--- webems_project/emulations/views.py
+++ webems_project/emulations/views.py
@@ -1,21 +1,62 @@
+from models import ExtendedFlatPage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 
 def mine(request):
     page = "My Emulations"
-    emulations = ""
+    emulations = ExtendedFlatPage.objects.all()
     return render_to_response('emulations.html', {'emulations' : emulations, 'page' : page}, context_instance=RequestContext(request))
 
 def all(request):
     page = "All Emulations"
-    emulations = ""
+    emulations = ExtendedFlatPage.objects.all()
     return render_to_response('emulations.html', {'emulations' : emulations, 'page' : page}, context_instance=RequestContext(request))
 
 def create(request):
     page = "Create"
     return render_to_response('create.html', {'page' : page}, context_instance=RequestContext(request))
 
-def edit(request):
-    page = "edit"
-    return render_to_response('create.html', {}, context_instance=RequestContext(request))
+def view(request, pk, url=""):
+    content = "Emulation " + pk + "does not exist. <a href='/''>Home</a>."
+    try:
+        f = ExtendedFlatPage.objects.get(id=pk)
+        content = f.content
+    except ExtendedFlatPage.DoesNotExist:
+        pass
+    except ExtendedFlatPage.MultipleObjectsReturned:
+        pass
+    return render_to_response('emulation.html', {'content' : content})
+
+def edit(request, pk, url=""):
+    content = "Emulation " + pk + "does not exist. <a href='/''>Home</a>."
+    try:
+        f = ExtendedFlatPage.objects.get(id=pk)
+        content = f.content
+    except ExtendedFlatPage.DoesNotExist:
+        pass
+    except ExtendedFlatPage.MultipleObjectsReturned:
+        pass
+    return render_to_response('emulation.html', {'content' : content})
+
+def delete(request, pk, url=""):
+    content = "Emulation " + pk + "does not exist. <a href='/''>Home</a>."
+    try:
+        f = ExtendedFlatPage.objects.get(id=pk)
+        content = f.content
+    except ExtendedFlatPage.DoesNotExist:
+        pass
+    except ExtendedFlatPage.MultipleObjectsReturned:
+        pass
+    return render_to_response('emulation.html', {'content' : content})
+
+def myemulations(request, pk, url=""):
+    content = "Emulation " + pk + "does not exist. <a href='/''>Home</a>."
+    try:
+        f = ExtendedFlatPage.objects.get(id=pk)
+        content = f.content
+    except ExtendedFlatPage.DoesNotExist:
+        pass
+    except ExtendedFlatPage.MultipleObjectsReturned:
+        pass
+    return render_to_response('emulation.html', {'content' : content})
--- webems_project/urls.py
+++ webems_project/urls.py
@@ -6,6 +6,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^', include('globals.urls')),
     url(r'^emulations/*', include('emulations.urls')),
+    url(r'emulation/(?P<pk>\d+)/view/$', 'emulations.views.view'),
+    url(r'emulation/(?P<pk>\d+)/edit/$', 'emulations.views.edit'),
+    url(r'emulation/(?P<pk>\d+)/delete/$', 'emulations.views.delete'),
+    url(r'emulation/(?P<pk>\d+)/myemulations/$', 'emulations.views.myemulations'),
     url(r'^accounts/*', include('accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
